0a1558ae0a6490e061190697e70736fe62d9f832
skinnypete65 <skinnypetetskipe@gmail.com>
2024-09-30 19:11:50 +0300

    hw4 task1_1 done

--- hw4/tasks1/task1/task1.go
+++ hw4/tasks1/task1/task1.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	num := 1234
+	fmt.Println(sumDigits(num))
+}
+
+// Ограничение на число - число обязательно четырехзначное
+func sumDigits(num int) int {
+	digit1 := num % 10
+	digit2 := (num / 10) % 10
+	digit3 := (num / 100) % 10
+	digit4 := (num / 1000) % 10
+
+	return digit1 + digit2 + digit3 + digit4
+}
